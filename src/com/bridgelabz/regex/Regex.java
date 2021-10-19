e3cadd7b422937c136185efffef21b7e5b27fe3c
riya40 <priyanunna20@gmail.com>
2021-10-19 08:51:45 +0530

    Added:Cecking the Last Name Pattern

--- src/com/bridgelabz/regex/Regex.java
+++ src/com/bridgelabz/regex/Regex.java
@@ -5,12 +5,26 @@ import java.util.regex.Pattern;
 public class Regex {
 
     public static void main(String[] args) {
+        //checking the pattern for first name
+        System.out.println("Printing the First Namae");
         Pattern pattern = Pattern.compile("^[A-Za-z]{3,6}$");
         Matcher matcher = pattern.matcher("Priya");
-        if (matcher.find())
+        if (matcher.find()) {
             System.out.println("Pattern matched");
-        else
+        }
+        else {
             System.out.println("Pattern not matched");
+        }
+        //checking the pattern for lasst name
+        System.out.println("Printing the last name");
+        Pattern pattern1 = Pattern.compile("^[A-Za-z]{3,6}$");
+        Matcher matcher1 = pattern.matcher("Nunna");
+        if (matcher.find()) {
+            System.out.println("Pattern matched");
+        }
+        else {
+            System.out.println("Pattern not matched");
+        }
 
     }
 }
